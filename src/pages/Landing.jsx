1836e1e49f1308ddc7265085e693aff4d99def92
will sherman <willsherman771@gmail.com>
2023-10-30 18:30:57 -0400

    sticks added

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -9,9 +9,11 @@ function Landing() {
 
             <div class='intro'>
                 <h1>Allow Myself to Introduce Myself...</h1>
+
             </div>
             <div class='man'>
                 <h2>The Man...</h2>
+                <img src="../assets/images/man.png" alt="stick figure man" height='50px' width='50px'/>
             </div>
             <div class='man'>
                 <h2>The Works...</h2>
