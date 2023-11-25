8becc402c298546e12d90752bd4d209e8aaebcab
will sherman <willsherman771@gmail.com>
2023-11-24 21:13:37 -0500

    text in theman

--- src/pages/TheMan.jsx
+++ src/pages/TheMan.jsx
@@ -1,10 +1,10 @@
 export default function TheMan() {
     return (
         <div>
-            <p>Hello! My name is Matthew Shuman. Thanks for taking the time to check out my page! Born in California, raised in Lancaster, came to Philly after Covid. I'm a life long cook, having done everything from flipping eggs at Cracker Barrel to being Sous at a 5 Star restaurant, Te at the Inn.  
+            <p>Hello! My name is Matthew Shuman. Thanks for taking the time to check out my page! Born in California, raised in Lancaster, came to Philly after Covid. I'm a life long cook, having done everything from flipping eggs at Cracker Barrel to being Sous Chef at a 5 Star restaurant, Te at the Inn, in Leola, PA 
             Looking for a new career path, I decided to pursue my love of computers and started an IT courses on Coursera. I then found the UPenn LPS Coding Bootcamp and decided to take the plunge. I'm excited to see where this new path takes me!
 
-            A dedicate team member with a passion for learning, I'm looking for a company that will allow me to grow and learn as a developer. An avid runner, I have completed several half marathons and one full marathon. The mental toughness and dedication required to complete a marathon is something I bring to every aspect of my life. I'm looking forward to bringing that same dedication to my new career.
+            A dedicate team member with a passion for learning, I'm looking for a company that will allow me to learn and grow as a developer. An avid runner, I have completed several half marathons and one full marathon. The mental toughness and dedication required to complete a marathon is something I bring to every aspect of my life. I'm looking forward to bringing that same dedication to my new career.
               </p>
         </div>
     );
