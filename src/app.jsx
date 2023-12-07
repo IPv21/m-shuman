ce875996d82c018774030a072a963c1024e73b73
will sherman <willsherman771@gmail.com>
2023-12-07 16:33:13 -0500

    images linked, folder structure changed

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Outlet,  } from 'react-router-dom';
-import Landing from './pages/Landing';
-import TheWork from './pages/TheWork';
-import TheMan from './pages/TheMan';
-import TheCall from './pages/TheCall';
+
 
 
 
--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import '../components/style.css'
+import '/src/style.css'
 
 function Landing() {
     function manClick () {
